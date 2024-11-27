313b470efa645b36b6b81e2da3f68bec2fc0498f
poojavadher <pooja@sanskartechnolab.com>
2024-11-27 17:44:56 +0530

    Initial Commit to add print formats

--- mediscient_custom/hooks.py
+++ mediscient_custom/hooks.py
@@ -242,3 +242,13 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    "Letter Head",
+    {"dt":"Print Format","filters":[
+        [
+        "module","in",[
+                "Mediscient Custom"
+            ]
+        ]
+    ]}
+]
